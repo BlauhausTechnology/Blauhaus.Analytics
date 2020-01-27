7b0160588e8745a4a93363786b6b9bb29b88a36c
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-01-27 19:48:43 +0200

    no message

--- src/Blauhaus.Analytics.Common/Service/BaseAnalyticsServerService.cs
+++ src/Blauhaus.Analytics.Common/Service/BaseAnalyticsServerService.cs
@@ -6,6 +6,7 @@ using Blauhaus.Analytics.Abstractions.Config;
 using Blauhaus.Analytics.Abstractions.Operation;
 using Blauhaus.Analytics.Abstractions.Service;
 using Blauhaus.Analytics.Abstractions.TelemetryClients;
+using Blauhaus.Analytics.Common.Extensions;
 using Blauhaus.Analytics.Console.ConsoleLoggers;
 using Blauhaus.Common.ValueObjects.BuildConfigs;
 using Microsoft.ApplicationInsights.DataContracts;
@@ -81,114 +82,26 @@ namespace Blauhaus.Analytics.Common.Service
         
         public void Trace(string message, LogSeverity logSeverity = LogSeverity.Verbose, Dictionary<string, object> properties = null)
         {
-
-            if (properties == null)
-            {
-                properties = new Dictionary<string, object>();
-            }
-
             if (Config.MinimumLogToServerSeverity.TryGetValue(CurrentBuildConfig, out var minumumSeverityToLogToServer))
             {
-                var stringifiedProperties = new Dictionary<string, string>();
-
                 if (logSeverity >= minumumSeverityToLogToServer)
                 {
-
-                    foreach (var property in properties)
-                    {
-                        if (property.Value != null)
-                        {
-                            if (property.Value is string stringValue)
-                            {
-                                stringifiedProperties[property.Key] = stringValue;
-                            }
-
-                            if (double.TryParse(property.Value.ToString(), out var numericValue))
-                            {
-                                stringifiedProperties[property.Key] = numericValue.ToString(CultureInfo.InvariantCulture);
-                            }
-
-                            else
-                            {
-                                stringifiedProperties[property.Key] = JsonConvert.SerializeObject(property.Value);
-                            }
-                        }
-                    }
-
-                    //don't log to server if min level is not exceeded
-                    TelemetryClient.TrackTrace(message, (SeverityLevel) logSeverity, stringifiedProperties);
-
+                    TelemetryClient.TrackTrace(message, (SeverityLevel) logSeverity, properties.ToDictionaryOfStrings());
                 }
-
-                ConsoleLogger.LogTrace(message, logSeverity, properties);
             }
+
+            ConsoleLogger.LogTrace(message, logSeverity, properties);
         }   
 
         public void LogEvent(string eventName, Dictionary<string, object> properties = null, Dictionary<string, double> metrics = null)
         {
-            if (properties == null)
-            {
-                properties = new Dictionary<string, object>();
-            }
-
-            var stringifiedProperties = new Dictionary<string, string>();
-
-            foreach (var property in properties)
-            {
-                if (property.Value != null)
-                {
-                    if(property.Value is string stringValue)
-                    {
-                        stringifiedProperties[property.Key] = stringValue;
-                    }
-
-                    if (double.TryParse(property.Value.ToString(), out var numericValue))
-                    {
-                        stringifiedProperties[property.Key] = numericValue.ToString(CultureInfo.InvariantCulture);
-                    }
-
-                    else
-                    {
-                        stringifiedProperties[property.Key] = JsonConvert.SerializeObject(property.Value);
-                    }
-                }
-            }
-                
-            TelemetryClient.TrackEvent(eventName, stringifiedProperties, metrics);
+            TelemetryClient.TrackEvent(eventName, properties.ToDictionaryOfStrings(), metrics);
             ConsoleLogger.LogEvent(eventName, properties, metrics);
         }
 
         public void LogException(Exception exception, Dictionary<string, object> properties = null, Dictionary<string, double> metrics = null)
         {
-            if (properties == null)
-            {
-                properties = new Dictionary<string, object>();
-            }
-
-            var stringifiedProperties = new Dictionary<string, string>();
-
-            foreach (var property in properties)
-            {
-                if (property.Value != null)
-                {
-                    if(property.Value is string stringValue)
-                    {
-                        stringifiedProperties[property.Key] = stringValue;
-                    }
-
-                    if (double.TryParse(property.Value.ToString(), out var numericValue))
-                    {
-                        stringifiedProperties[property.Key] = numericValue.ToString(CultureInfo.InvariantCulture);
-                    }
-
-                    else
-                    {
-                        stringifiedProperties[property.Key] = JsonConvert.SerializeObject(property.Value);
-                    }
-                }
-            }
-                
-            TelemetryClient.TrackException(exception, stringifiedProperties, metrics);
+            TelemetryClient.TrackException(exception, properties.ToDictionaryOfStrings(), metrics);
             ConsoleLogger.LogException(exception, properties, metrics);
         }
     }

a9b0cb90b3f37417b94fd2914f1fdc8608d7ca17
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-02-29 11:12:52 +0200

    added blazor sample

---
+++ src/Samples/Blauhaus.Analytics.Samples.BlazorServer/Startup.cs
@@ -0,0 +1,71 @@
+using System.Diagnostics;
+using Blauhaus.Analytics.Abstractions.Service;
+using Blauhaus.Analytics.AspNetCore._Ioc;
+using Blauhaus.Analytics.Samples.BlazorServer._Config;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Blauhaus.Analytics.Samples.BlazorServer.Data;
+using Blauhaus.Common.ValueObjects.BuildConfigs;
+
+namespace Blauhaus.Analytics.Samples.BlazorServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            RegisterBuildConfig(services);
+
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+
+            services.RegisterAspNetCoreAnalyticsService<AnalyticsConfig>(new ConsoleTraceListener());
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IAnalyticsService analyticsService)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+
+            analyticsService.Trace(this, "Server Started");
+        }
+
+        private static void RegisterBuildConfig(IServiceCollection services)
+        {
+            #if DEBUG
+            services.AddSingleton<IBuildConfig>(BuildConfig.Debug);
+            #else
+            services.AddSingleton<IBuildConfig>(BuildConfig.Release);
+            #endif
+        }
+
+    }
+}
---
+++ src/Samples/Blauhaus.Analytics.Samples.BlazorServer/_Config/AnalyticsConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Blauhaus.Analytics.Abstractions.Config;
+using Blauhaus.Analytics.Abstractions.Service;
+using Blauhaus.Common.ValueObjects.BuildConfigs;
+using Microsoft.Extensions.Configuration;
+
+namespace Blauhaus.Analytics.Samples.BlazorServer._Config
+{
+    public class AnalyticsConfig : BaseApplicationInsightsConfig
+    {
+        public AnalyticsConfig(IConfiguration configuration) : base("4e647a09-071e-4cb8-85e4-303d5458d6fb", "Blazor Server")
+        {
+            MinimumLogToServerSeverity[BuildConfig.Debug] = LogSeverity.Critical;
+            MinimumLogToServerSeverity[BuildConfig.Release] = LogSeverity.Information;
+        }
+    }
+}

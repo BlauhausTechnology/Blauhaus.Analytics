1f5ada7c40ec597719b6b0b83886faf53212e0ff
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-03-02 14:04:49 +0200

    no message

--- src/Blauhaus.Analytics.Xamarin/_Ioc/IocServiceExtensions.cs
+++ src/Blauhaus.Analytics.Xamarin/_Ioc/IocServiceExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Blauhaus.Analytics.Abstractions.Config;
 using Blauhaus.Analytics.Abstractions.Service;
 using Blauhaus.Analytics.Abstractions.Session;
+using Blauhaus.Analytics.Common.Service;
 using Blauhaus.Analytics.Common.Telemetry;
 using Blauhaus.Analytics.Console._Ioc;
 using Blauhaus.Analytics.Xamarin.SessionFactories;
@@ -28,6 +29,7 @@ namespace Blauhaus.Analytics.Xamarin._Ioc
             iocService.RegisterImplementation<ITelemetryDecorator, TelemetryDecorator>();
 
             iocService.RegisterImplementation<IAnalyticsSessionFactory, XamarinSessionFactory>(IocLifetime.Singleton);
+            iocService.RegisterImplementation<IAnalyticsService, AnalyticsService>();
 
             return iocService;
         }

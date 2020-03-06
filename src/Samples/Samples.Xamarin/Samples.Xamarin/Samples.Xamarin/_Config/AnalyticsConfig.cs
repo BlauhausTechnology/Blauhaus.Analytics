d8ac0982cb648e75dcf61f75bf705eff1d125c18
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-03-06 08:08:26 +0200

    config

---
+++ src/Samples/Samples.Xamarin/Samples.Xamarin/Samples.Xamarin/_Config/AnalyticsConfig.cs
@@ -0,0 +1,15 @@
+﻿using Blauhaus.Analytics.Abstractions.Config;
+using Blauhaus.Analytics.Abstractions.Service;
+using Blauhaus.Common.ValueObjects.BuildConfigs;
+
+namespace Samples.Xamarin._Config
+{
+    public class AnalyticsConfig : BaseApplicationInsightsConfig
+    {
+        public AnalyticsConfig() : base("4e647a09-071e-4cb8-85e4-303d5458d6fb", "Xamarin App")
+        {
+            MinimumLogToServerSeverity[BuildConfig.Debug] = LogSeverity.Verbose;
+            MinimumLogToServerSeverity[BuildConfig.Release] = LogSeverity.Verbose;
+        }
+    }
+}

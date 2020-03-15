7205707c0a0b925f54cdfa1d40d6c17fa7a34704
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-03-15 11:40:45 +0200

    added extensions and test helpers project

---
+++ src/Blauhaus.Analytics.Abstractions/Extensions/ObjectExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Blauhaus.Analytics.Abstractions.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static Dictionary<string, object> ToObjectDictionary(this object value, string key)
+        {
+            return new Dictionary<string, object>
+            {
+                {key, value }
+            };
+        }
+    }
+}
---
+++ src/Blauhaus.Analytics.TestHelpers/AnalyticsServiceMockBuilder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Blauhaus.Analytics.Abstractions.Service;
+using Blauhaus.Common.TestHelpers;
+using Moq;
+
+namespace Blauhaus.Analytics.TestHelpers
+{
+     public class AnalyticsServiceMockBuilder : BaseMockBuilder<AnalyticsServiceMockBuilder, IAnalyticsService>
+    {
+        public void VerifyLogEvent(string eventName)
+        {
+            Mock.Verify(x => x.LogEvent(It.IsAny<object>(), eventName, It.IsAny<Dictionary<string, object>>(), It.IsAny<Dictionary<string, double>>(), It.IsAny<string>()));
+        }
+
+        public void VerifyTrace(string traceMessage, LogSeverity severity)
+        {
+            Mock.Verify(x => x.Trace(It.IsAny<object>(), traceMessage, severity, It.IsAny<Dictionary<string, object>>(), It.IsAny<string>()));
+        }
+        
+        public void VerifyTraceProperty<T>(string key, T value) where T : class
+        {
+            Mock.Verify(x => x.Trace(It.IsAny<object>(), It.IsAny<string>(), It.IsAny<LogSeverity>(), 
+                It.Is<Dictionary<string, object>>(y => 
+                    (y[key] as T).Equals(value)), 
+                It.IsAny<string>()));
+        }
+
+        
+        public void VerifyEventProperty<T>(string key, T value) where T : class
+        {
+            Mock.Verify(x => x.LogEvent(It.IsAny<object>(), It.IsAny<string>(), 
+                It.Is<Dictionary<string, object>>(y => 
+                    ((T)y[key] ).Equals(value)), 
+                It.IsAny<Dictionary<string, double>>(), It.IsAny<string>()));
+        }
+
+        public void VerifyEventProperty<T>(string key, Expression<Func<T,bool>> predicate) where T : class
+        {
+            Mock.Verify(x => x.LogEvent(It.IsAny<object>(), It.IsAny<string>(), 
+                It.Is<Dictionary<string, object>>(y => 
+                    (y[key] as T) == It.Is<T>(predicate)), 
+                It.IsAny<Dictionary<string, double>>(), It.IsAny<string>()));
+        }
+
+
+        public void VerifyEventProperty(Expression<Func<Dictionary<string, object>, bool>> match) 
+        {
+            Mock.Verify(x => x.LogEvent(It.IsAny<object>(), It.IsAny<string>(), 
+                It.Is(match), 
+                It.IsAny<Dictionary<string, double>>(), It.IsAny<string>()));
+        }
+    }
+}

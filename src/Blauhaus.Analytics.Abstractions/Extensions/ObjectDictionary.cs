e2540f4c0771c7e1126fc584a71ec9f1654587d6
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-03-16 13:22:01 +0200

    more dictionary extensions

---
+++ src/Blauhaus.Analytics.Abstractions/Extensions/ObjectDictionary.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Blauhaus.Analytics.Abstractions.Extensions
+{
+    public class ObjectDictionary : Dictionary<string, object>
+    {
+        public ObjectDictionary(params object[] values)
+        {
+            this.WithValues(values);
+        }
+        
+        public ObjectDictionary(IEnumerable<object> values)
+        {
+            this.WithValues(values);
+        }
+    }
+}
--- src/Blauhaus.Analytics.Abstractions/Extensions/ObjectDictionaryExtensions.cs
+++ src/Blauhaus.Analytics.Abstractions/Extensions/ObjectDictionaryExtensions.cs
@@ -10,12 +10,36 @@ namespace Blauhaus.Analytics.Abstractions.Extensions
             return dictionary;
         }
 
+        public static Dictionary<string, object> WithValue(this Dictionary<string, object> dictionary, object value)
+        {
+            dictionary[value.GetType().Name] = value;
+            return dictionary;
+        }
+
         
         public static Dictionary<string, object> WithValues(this Dictionary<string, object> dictionary, Dictionary<string, string> values)
         {
             foreach (var value in values)
             {
                 dictionary[value.Key] = value.Value;
+            }
+            return dictionary;
+        }
+
+        
+        public static Dictionary<string, object> WithValues(this Dictionary<string, object> dictionary, IEnumerable<object> values)
+        {
+            foreach (var value in values)
+            {
+                dictionary[value.GetType().Name] = value;
+            }
+            return dictionary;
+        }
+        public static Dictionary<string, object> WithValues(this Dictionary<string, object> dictionary, params object[] values)
+        {
+            foreach (var value in values)
+            {
+                dictionary[value.GetType().Name] = value;
             }
             return dictionary;
         }

9ca56419a9238f5e4d886768e6faeb21f6e4efcc
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-04 15:28:25 +0200

    Error

---
+++ src/Blauhaus.Analytics.Abstractions/Errors/Error.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Blauhaus.Analytics.Abstractions.Errors
+{
+    public class Error : IEquatable<Error>
+    {
+
+        private Error(string code, string description)
+        {
+            Code = code;
+            Description = description;
+        }
+
+        public Error(string description)
+        {
+            Code = GetType().Name;
+            Description = description;
+        }
+
+        public string Code { get; }
+        public string Description { get; }
+
+        public override string ToString()
+        {
+            return $"{Code} ::: {Description}";
+        }
+
+        public static Error Deserialize(string serializedError)
+        {
+            var deserialized = serializedError.Split(new []{" ::: "}, StringSplitOptions.None);
+            if (deserialized.Length != 2)
+            {
+                throw new ArgumentException($"Input {serializedError} is not a valid serialized Error");
+            }
+            return new Error(deserialized[0], deserialized[1]);
+        }
+
+        public bool Equals(Error? other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return Code == other.Code && Description == other.Description;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return Equals((Error) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Code.GetHashCode() * 397) ^ Description.GetHashCode();
+            }
+        }
+
+        public static bool operator ==(Error? left, Error? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Error? left, Error? right)
+        {
+            return !Equals(left, right);
+        }
+    }
+
+
+    public static class StringExtensions
+    {
+        public static Error ToError(this string serializedError)
+        {
+            return Error.Deserialize(serializedError);
+        }
+    }
+}
--- src/Blauhaus.Analytics.Abstractions/Extensions/AnalyticsServiceExtensions.cs
+++ src/Blauhaus.Analytics.Abstractions/Extensions/AnalyticsServiceExtensions.cs
@@ -88,5 +88,20 @@ namespace Blauhaus.Analytics.Abstractions.Extensions
             analyticsService.Trace(sender, message, LogSeverity.Error, new Dictionary<string, object>{{propertyName, propertyValue}}, caller);
             return analyticsService;
         }
+        public static string TraceError(this IAnalyticsService analyticsService, object sender, Error error, [CallerMemberName] string caller = "")
+        {
+            analyticsService.Trace(sender, error.Code, LogSeverity.Error, new Dictionary<string, object>(), caller);
+            return error.ToString();
+        }
+        public static string TraceError(this IAnalyticsService analyticsService, object sender, Error error, object property, [CallerMemberName] string caller = "")
+        {
+            analyticsService.Trace(sender, error.Code, LogSeverity.Error, property.ToObjectDictionary(), caller);
+            return error.ToString();
+        }
+        public static string TraceError(this IAnalyticsService analyticsService, object sender, Error error, string propertyName, string propertyValue, [CallerMemberName] string caller = "")
+        {
+            analyticsService.Trace(sender, error.Code, LogSeverity.Error, new Dictionary<string, object>{{propertyName, propertyValue}}, caller);
+            return error.ToString();
+        }
     }
 }

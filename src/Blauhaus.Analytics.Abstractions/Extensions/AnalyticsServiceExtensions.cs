851bec9075b2c804b3100d2e76ca2412630f5fd8
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-11 17:47:14 +0200

    no message

--- src/Blauhaus.Analytics.Abstractions/Extensions/AnalyticsServiceExtensions.cs
+++ src/Blauhaus.Analytics.Abstractions/Extensions/AnalyticsServiceExtensions.cs
@@ -13,21 +13,6 @@ namespace Blauhaus.Analytics.Abstractions.Extensions
             analyticsService.Trace(sender, message, LogSeverity.Verbose, new Dictionary<string, object>(), caller);
             return analyticsService;
         }
-
-        public static IAnalyticsService TraceVerbose(this IAnalyticsService analyticsService, object sender, string message, object property, [CallerMemberName] string caller = "")
-        {
-            if (property is Dictionary<string, object> objectProperties)
-            {
-                analyticsService.Trace(sender, message, LogSeverity.Verbose, objectProperties, caller);
-            }
-            else
-            {
-                analyticsService.Trace(sender, message, LogSeverity.Verbose, property.ToObjectDictionary(), caller);
-            }
-
-            return analyticsService;
-        }
-
         public static IAnalyticsService TraceVerbose(this IAnalyticsService analyticsService, object sender, string message, string propertyName, string propertyValue, [CallerMemberName] string caller = "")
         {
             analyticsService.Trace(sender, message, LogSeverity.Verbose, new Dictionary<string, object> {{propertyName, propertyValue}}, caller);
@@ -38,22 +23,7 @@ namespace Blauhaus.Analytics.Abstractions.Extensions
         {
             analyticsService.Trace(sender, message, LogSeverity.Information, new Dictionary<string, object>(), caller);
             return analyticsService;
-        }
-
-        public static IAnalyticsService TraceInformation(this IAnalyticsService analyticsService, object sender, string message, object property, [CallerMemberName] string caller = "")
-        {
-            if (property is Dictionary<string, object> objectProperties)
-            {
-                analyticsService.Trace(sender, message, LogSeverity.Information, objectProperties, caller);
-            }
-            else
-            {
-                analyticsService.Trace(sender, message, LogSeverity.Information, property.ToObjectDictionary(), caller);
-            }
-
-            return analyticsService;
-        }
-
+        } 
         public static IAnalyticsService TraceInformation(this IAnalyticsService analyticsService, object sender, string message, string propertyName, string propertyValue, [CallerMemberName] string caller = "")
         {
             analyticsService.Trace(sender, message, LogSeverity.Information, new Dictionary<string, object> {{propertyName, propertyValue}}, caller);
@@ -66,21 +36,6 @@ namespace Blauhaus.Analytics.Abstractions.Extensions
             analyticsService.Trace(sender, message, LogSeverity.Warning, new Dictionary<string, object>(), caller);
             return analyticsService;
         }
-
-        public static IAnalyticsService TraceWarning(this IAnalyticsService analyticsService, object sender, string message, object property, [CallerMemberName] string caller = "")
-        {
-            if (property is Dictionary<string, object> objectProperties)
-            {
-                analyticsService.Trace(sender, message, LogSeverity.Warning, objectProperties, caller);
-            }
-            else
-            {
-                analyticsService.Trace(sender, message, LogSeverity.Warning, property.ToObjectDictionary(), caller);
-            }
-
-            return analyticsService;
-        }
-
         public static IAnalyticsService TraceWarning(this IAnalyticsService analyticsService, object sender, string message, string propertyName, string propertyValue, [CallerMemberName] string caller = "")
         {
             analyticsService.Trace(sender, message, LogSeverity.Warning, new Dictionary<string, object> {{propertyName, propertyValue}}, caller);
@@ -93,21 +48,6 @@ namespace Blauhaus.Analytics.Abstractions.Extensions
             analyticsService.Trace(sender, message, LogSeverity.Critical, new Dictionary<string, object>(), caller);
             return analyticsService;
         }
-
-        public static IAnalyticsService TraceCritical(this IAnalyticsService analyticsService, object sender, string message, object property, [CallerMemberName] string caller = "")
-        {
-            if (property is Dictionary<string, object> objectProperties)
-            {
-                analyticsService.Trace(sender, message, LogSeverity.Critical, objectProperties, caller);
-            }
-            else
-            {
-                analyticsService.Trace(sender, message, LogSeverity.Critical, property.ToObjectDictionary(), caller);
-            }
-
-            return analyticsService;
-        }
-
         public static IAnalyticsService TraceCritical(this IAnalyticsService analyticsService, object sender, string message, string propertyName, string propertyValue, [CallerMemberName] string caller = "")
         {
             analyticsService.Trace(sender, message, LogSeverity.Critical, new Dictionary<string, object> {{propertyName, propertyValue}}, caller);
@@ -120,21 +60,6 @@ namespace Blauhaus.Analytics.Abstractions.Extensions
             analyticsService.Trace(sender, message, LogSeverity.Error, new Dictionary<string, object>(), caller);
             return analyticsService;
         }
-
-        public static IAnalyticsService TraceError(this IAnalyticsService analyticsService, object sender, string message, object property, [CallerMemberName] string caller = "")
-        {
-            if (property is Dictionary<string, object> objectProperties)
-            {
-                analyticsService.Trace(sender, message, LogSeverity.Error, objectProperties, caller);
-            }
-            else
-            {
-                analyticsService.Trace(sender, message, LogSeverity.Error, property.ToObjectDictionary(), caller);
-            }
-
-            return analyticsService;
-        }
-
         public static IAnalyticsService TraceError(this IAnalyticsService analyticsService, object sender, string message, string propertyName, string propertyValue, [CallerMemberName] string caller = "")
         {
             analyticsService.Trace(sender, message, LogSeverity.Error, new Dictionary<string, object> {{propertyName, propertyValue}}, caller);
@@ -147,15 +72,7 @@ namespace Blauhaus.Analytics.Abstractions.Extensions
         {
             analyticsService.Trace(sender, error.Code, logSeverity, new Dictionary<string, object>(), caller);
             return error.ToString();
-        }
-
-        public static string TraceError(this IAnalyticsService analyticsService, object sender, Error error, object property,
-            LogSeverity logSeverity = LogSeverity.Error, [CallerMemberName] string caller = "")
-        {
-            analyticsService.Trace(sender, error.Code, logSeverity, property.ToObjectDictionary(), caller);
-            return error.ToString();
-        }
-
+        } 
         public static string TraceError(this IAnalyticsService analyticsService, object sender, Error error, string propertyName, string propertyValue,
             LogSeverity logSeverity = LogSeverity.Error, [CallerMemberName] string caller = "")
         {

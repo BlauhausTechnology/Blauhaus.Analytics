f9b22aa05ff1704469cf9c0dea0dc823c607f2ec
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-08-14 06:35:27 +0200

    no message

--- src/Blauhaus.Analytics.TestHelpers/Extensions/ResultExtensions.cs
+++ src/Blauhaus.Analytics.TestHelpers/Extensions/ResultExtensions.cs
@@ -11,25 +11,25 @@ namespace Blauhaus.Analytics.TestHelpers.Extensions
     {
         public static void VerifyResultError(this Result result, Error error, AnalyticsServiceMockBuilder mockAnalyticsService)
         {
-            Assert.That(result.Error.ToError().Code, Is.EqualTo(error.Code));
+            Assert.That(result.Error.ToError(), Is.EqualTo(error));
             mockAnalyticsService.VerifyTrace(error.Code, LogSeverity.Error);
         }
 
-        public static void VerifyResultError<T>(this Result<T> result, Error error, AnalyticsServiceMockBuilder mockAnalyticsService)
-        {
-            Assert.That(result.Error.ToError().Code, Is.EqualTo(error.Code));
-            mockAnalyticsService.VerifyTrace(error.Code, LogSeverity.Error);
-        }
+        //public static void VerifyResultError<T>(this Result<T> result, Error error, AnalyticsServiceMockBuilder mockAnalyticsService)
+        //{
+        //    Assert.That(result.Error.ToError().Code, Is.EqualTo(error.Code));
+        //    mockAnalyticsService.VerifyTrace(error.Code, LogSeverity.Error);
+        //}
 
-        public static void VerifyResultException<T>(this Result<T> result, Error error, string exceptionMessage, AnalyticsServiceMockBuilder mockAnalyticsService)
-        {
-            Assert.That(result.Error.ToError().Code, Is.EqualTo(error.Code));
-            mockAnalyticsService.VerifyLogExceptionWithMessage(exceptionMessage);
-        }
+        //public static void VerifyResultException<T>(this Result<T> result, Error error, string exceptionMessage, AnalyticsServiceMockBuilder mockAnalyticsService)
+        //{
+        //    Assert.That(result.Error.ToError().Code, Is.EqualTo(error.Code));
+        //    mockAnalyticsService.VerifyLogExceptionWithMessage(exceptionMessage);
+        //}
 
         public static void VerifyResultException(this Result result, Error error, string exceptionMessage, AnalyticsServiceMockBuilder mockAnalyticsService)
         {
-            Assert.That(result.Error.ToError().Code, Is.EqualTo(error.Code));
+            Assert.That(result.Error.ToError(), Is.EqualTo(error));
             mockAnalyticsService.VerifyLogExceptionWithMessage(exceptionMessage);
         }
     }

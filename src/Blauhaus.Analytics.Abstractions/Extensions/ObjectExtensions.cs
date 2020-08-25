56a4660a0e7dfcfd11c255c8c8ef4d9bbc6ec2f8
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-08-25 09:48:47 +0200

    no message

--- src/Blauhaus.Analytics.Abstractions/Extensions/ObjectExtensions.cs
+++ src/Blauhaus.Analytics.Abstractions/Extensions/ObjectExtensions.cs
@@ -4,9 +4,14 @@ namespace Blauhaus.Analytics.Abstractions.Extensions
 {
     public static class ObjectExtensions
     {
-        public static Dictionary<string, object> ToObjectDictionary(this object value, string key = "")
+        public static Dictionary<string, object> ToObjectDictionary(this object? value, string key = "")
         {
-
+            if (value == null)
+            {
+                return string.IsNullOrEmpty(key) 
+                    ? new Dictionary<string, object>() 
+                    : new Dictionary<string, object>{{key, "NULL"}};
+            }
             if (string.IsNullOrWhiteSpace(key))
             {
                 key = value.GetType().Name;

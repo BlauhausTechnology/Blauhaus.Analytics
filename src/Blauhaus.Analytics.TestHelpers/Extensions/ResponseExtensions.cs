5b60312175d01280b90e12dcc8f4e5f737cfef6d
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-11-14 10:03:15 +0200

    no message

--- src/Blauhaus.Analytics.TestHelpers/Extensions/ResponseExtensions.cs
+++ src/Blauhaus.Analytics.TestHelpers/Extensions/ResponseExtensions.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using Blauhaus.Analytics.Abstractions.Service;
 using Blauhaus.Analytics.TestHelpers.MockBuilders;
 using Blauhaus.Errors;
 using Blauhaus.Errors.Extensions;
 using Blauhaus.Responses;
+using Moq;
 using NUnit.Framework;
 
 namespace Blauhaus.Analytics.TestHelpers.Extensions
@@ -14,13 +16,13 @@ namespace Blauhaus.Analytics.TestHelpers.Extensions
         public static void VerifyResponseError(this Response result, Error error, AnalyticsServiceMockBuilder mockAnalyticsService, LogSeverity logSeverity = LogSeverity.Error)
         {
             Assert.That(result.Error.Equals(error));
-            mockAnalyticsService.VerifyTrace(error.ToString(), logSeverity);
+            mockAnalyticsService.Mock.Verify(x => x.Trace(It.IsAny<object>(), It.Is<string>(y => y.StartsWith(error.Code)), logSeverity, It.IsAny<Dictionary<string, object>>(), It.IsAny<string>()));
         }
 
         public static void VerifyResponseError<T>(this Response<T> result, Error error, AnalyticsServiceMockBuilder mockAnalyticsService, LogSeverity logSeverity = LogSeverity.Error)
         {
             Assert.That(result.Error.Equals(error));
-            mockAnalyticsService.VerifyTrace(error.ToString(), logSeverity);
+            mockAnalyticsService.Mock.Verify(x => x.Trace(It.IsAny<object>(), It.Is<string>(y => y.StartsWith(error.Code)), logSeverity, It.IsAny<Dictionary<string, object>>(), It.IsAny<string>()));
         }
 
         public static void VerifyResponseException<T>(this Response<T> result, Error error, string exceptionMessage, AnalyticsServiceMockBuilder mockAnalyticsService)
@@ -34,6 +36,7 @@ namespace Blauhaus.Analytics.TestHelpers.Extensions
             Assert.That(result.Error.Equals(error));
             mockAnalyticsService.VerifyLogExceptionWithMessage(exceptionMessage);
         }
+
         public static void VerifyResponseException(this Response result, Error error, Expression<Func<Exception, bool>> predicate, AnalyticsServiceMockBuilder mockAnalyticsService)
         {
             Assert.That(result.Error.Equals(error));

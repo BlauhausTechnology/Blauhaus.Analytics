bccff7863ceb3410dd14b19bf8987d5229f2f1dd
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-10-06 15:18:44 +0200

    no message

--- src/Blauhaus.Analytics.Console/ConsoleLoggers/ConsoleColours.cs
+++ src/Blauhaus.Analytics.Console/ConsoleLoggers/ConsoleColours.cs
@@ -16,9 +16,11 @@ namespace Blauhaus.Analytics.Console.ConsoleLoggers
             {LogSeverity.Critical, ConsoleColor.Red },
         };
 
+
         public static ConsoleColor EventColour { get; } = ConsoleColor.Magenta;
         public static ConsoleColor ExceptionColour { get; } = ConsoleColor.Red;
         public static ConsoleColor OperationColour { get; } = ConsoleColor.DarkYellow;
+
 
     }
 }

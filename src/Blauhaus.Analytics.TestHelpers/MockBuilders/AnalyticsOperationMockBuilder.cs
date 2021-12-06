c252ab2d0692584ba07f760467a0b710259431bf
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-12-06 14:53:05 +0200

    no message

--- src/Blauhaus.Analytics.TestHelpers/MockBuilders/AnalyticsOperationMockBuilder.cs
+++ src/Blauhaus.Analytics.TestHelpers/MockBuilders/AnalyticsOperationMockBuilder.cs
@@ -1,10 +1,15 @@
-﻿using Blauhaus.Analytics.Abstractions.Operation;
+﻿using System;
+using Blauhaus.Analytics.Abstractions.Operation;
 using Blauhaus.TestHelpers.MockBuilders;
 
 namespace Blauhaus.Analytics.TestHelpers.MockBuilders
 {
     public class AnalyticsOperationMockBuilder : BaseMockBuilder<AnalyticsOperationMockBuilder, IAnalyticsOperation>
     {
-        
+        public AnalyticsOperationMockBuilder()
+        {
+            With(x => x.Id, Guid.NewGuid().ToString());
+            With(x => x.Name, $"Operation {Guid.NewGuid()}");
+        }
     }
 }

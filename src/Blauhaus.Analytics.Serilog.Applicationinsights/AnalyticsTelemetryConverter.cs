c59c19df21258c87294aa1837df8eb79a0273280
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-03-23 15:30:52 +0200

    no message

--- src/Blauhaus.Analytics.Serilog.Applicationinsights/AnalyticsTelemetryConverter.cs
+++ src/Blauhaus.Analytics.Serilog.Applicationinsights/AnalyticsTelemetryConverter.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.ApplicationInsights.Channel;
-using Microsoft.ApplicationInsights.DataContracts;
+﻿using Microsoft.ApplicationInsights.DataContracts;
 using Serilog.Events;
 using Serilog.Sinks.ApplicationInsights.Sinks.ApplicationInsights.TelemetryConverters;
-using System.Collections.Generic;
 using System;
-using System.Linq;
 
 public class AnalyticsTelemetryConverter : TraceTelemetryConverter
 { 

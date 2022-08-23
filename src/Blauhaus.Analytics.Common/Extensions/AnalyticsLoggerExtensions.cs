1b782b0f5c2bd0b2d506071299f444014cd14e87
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-08-23 09:44:03 +0200

    no message

--- src/Blauhaus.Analytics.Common/Extensions/AnalyticsLoggerExtensions.cs
+++ src/Blauhaus.Analytics.Common/Extensions/AnalyticsLoggerExtensions.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Blauhaus.Auth.Abstractions.Errors;
 using Blauhaus.Errors;
 using Blauhaus.Responses;
 using Microsoft.Extensions.Logging;
@@ -31,11 +33,29 @@ public static class AnalyticsLoggerExtensions
     }
     private static Error ExctractNetworkError(this Exception exception)
     {
+        if (exception is ErrorException errorException)
+        {
+            return errorException.Error;
+        }
+
+        var message = exception.Message;
+        
+        if (exception is HttpRequestException)
+        {
+            if (message.Contains("401") || message.Contains("403"))
+                return AuthError.NotAuthorized;
+            
+            if(message.Contains("500"))
+                return Error.Unexpected();
+            
+            return Error.Unexpected($"Unexpected Http error: {message}");
+        }
+        
         var error = Error.Unexpected("Unable to complete network request");
 
-        if (exception is TaskCanceledException cancelledException)
+        if (exception is TaskCanceledException)
         {
-            error = cancelledException.Message.StartsWith("The request was canceled due to the configured HttpClient.Timeout") 
+            error = message.StartsWith("The request was canceled due to the configured HttpClient.Timeout") 
                 ? Error.Timeout 
                 : Error.Cancelled;
         }
